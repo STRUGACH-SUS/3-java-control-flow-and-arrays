16d3d261f65aa5ae7dd6db879564a2b47e2236ff
stryg <mitakervanov@gmail.com>
2026-09-08 03:07:29 +0500

    Выполнил 10 задание

--- task10/src/main/java/com/example/task10/Task10Main.java
+++ task10/src/main/java/com/example/task10/Task10Main.java
@@ -2,17 +2,25 @@ package com.example.task10;
 
 public class Task10Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         int[] arr = {7, 5, 9};
         System.out.println(numMin(arr));
-         */
+
     }
 
     static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int min = arr[0];
+        int count = -1;
+        int count_min = 0;
+
+        for (int n : arr) {
+            count++;
+            if (n <= min) {
+                min = n;
+                count_min = count;
+            }
+        }
+
+        return count_min;
     }
 
 }
